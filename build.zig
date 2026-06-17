6ca560ed317a06b85c93935066c3eadc85b1558f
2nayci <can.nayci@studium.uni-hamburg.de>
2026-06-17 21:43:39 +0200

    chore: zls check build step

--- build.zig
+++ build.zig
@@ -5,12 +5,14 @@ const build_spirv = @import("tools/build_spirv.zig");
 const cdep_transl = @import("tools/cdep_transl.zig");
 const CDependency = cdep_transl.CDependency;
 
+const name: []const u8 = "rpg";
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "rpg",
+        .name = name,
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
@@ -32,4 +34,11 @@ pub fn build(b: *std.Build) void {
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
+
+    const exe_check = b.addExecutable(.{
+        .name = name,
+        .root_module = exe.root_module,
+    });
+    const check = b.step("check", "Check if ++ \"" ++ name ++ "\" compiles");
+    check.dependOn(&exe_check.step);
 }
